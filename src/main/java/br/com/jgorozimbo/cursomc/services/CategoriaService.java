e7341c61059a939676700f61d202609c0e091e15
Josemar Orozimbo <jgorozimbo@gmail.com>
2018-06-11 21:47:58 -0300

    Criando repository e service para categoria

---
+++ src/main/java/br/com/jgorozimbo/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package br.com.jgorozimbo.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.jgorozimbo.cursomc.domain.Categoria;
+import br.com.jgorozimbo.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
