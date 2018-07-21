552b282173e43a326f0dcd0d06fbc5215649bf3d
Josemar Orozimbo <jgorozimbo@gmail.com>
2018-07-21 08:41:12 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

---
+++ src/main/java/br/com/jgorozimbo/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,42 @@
+package br.com.jgorozimbo.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import br.com.jgorozimbo.cursomc.domain.enums.TipoCliente;
+import br.com.jgorozimbo.cursomc.dto.ClienteNewDTO;
+import br.com.jgorozimbo.cursomc.resources.exception.FieldMessage;
+import br.com.jgorozimbo.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO>{
+
+	@Override
+	public void initialize(ClienteInsert constraintAnnotation) {
+	}
+	
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		// inclua os testes aqui, inserindo erros na lista
+		
+		if  (objDto.getTipo().equals(TipoCliente.PESSOA_FISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj","CPF inválido"));
+		}
+		
+		if  (objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj","CNPJ inválido"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName()).addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+
+
+}
